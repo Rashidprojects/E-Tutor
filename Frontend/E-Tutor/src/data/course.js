a941dc8181192524837afa234005d6ea84c18a53
Rashidprojects <rashidgitproject@gmail.com>
2024-09-02 23:13:16 +0530

    Course Category Large screen design completed

---
+++ Frontend/E-Tutor/src/data/course.js
@@ -0,0 +1,90 @@
+import label from '../../src/assets/course-logos/Frame 145.png'
+import business from '../../src/assets/course-logos/Frame Business.jpg'
+import finance from '../../src/assets/course-logos/Frame Finance.png'
+import it from '../../src/assets/course-logos/Frame It.png'
+import personal from '../../src/assets/course-logos/Frame Personal Dev.png'
+import office from '../../src/assets/course-logos/Frame Office Prdvty.png'
+import marketing from '../../src/assets/course-logos/Frame Marketing.png'
+import photo from '../../src/assets/course-logos/Frame photography.png'
+import lifestyle from '../../src/assets/course-logos/Frame lifestyle.png'
+import design from '../../src/assets/course-logos/Frame Design.png'
+import health from '../../src/assets/course-logos/Frame Fitness.png'
+import music from '../../src/assets/course-logos/Frame Music.png'
+
+
+const coursesData = [
+    {
+      name: 'Label',
+      bg : '#EBEBFF',
+      logo: label,
+      noOfCourse: 100,
+    },
+    {
+        name: 'Business',
+        bg : '#E1F7E3',
+        logo: business,
+        noOfCourse: 250,
+      },
+      {
+        name: 'Finance & Accounting',
+        bg : '#FFF2E5',
+        logo: finance,
+        noOfCourse: 140,
+      },
+      {
+        name: 'IT & Software',
+        bg : '#FFF0F0',
+        logo: it,
+        noOfCourse: 157,
+      },
+      {
+        name: 'Personal Development',
+        bg : '#FFFFFF',
+        logo: personal,
+        noOfCourse: 122,
+      },
+      {
+        name: 'Office Productivity',
+        bg : '#F5F7FA',
+        logo: office,
+        noOfCourse: 167,
+      },
+      {
+        name: 'Marketing',
+        bg : '#EBEBFF',
+        logo: marketing,
+        noOfCourse: 189,
+      },
+      {
+        name: 'Photography and Video',
+        bg : '#F5F7FA',
+        logo: photo,
+        noOfCourse: 146,
+      },
+      {
+        name: 'Lifestyle',
+        bg : '#FFF2E5',
+        logo: lifestyle,
+        noOfCourse: 169,
+      },
+      {
+        name: 'Design',
+        bg : '#FFEEE8',
+        logo: design,
+        noOfCourse: 225,
+      },
+      {
+        name: 'Health and fitness',
+        bg : '#E1F7E3',
+        logo: health,
+        noOfCourse: 96,
+      },
+      {
+        name: 'Music',
+        bg : '#FFF2E5',
+        logo: music,
+        noOfCourse: 128,
+      },
+  ]
+
+export default coursesData
